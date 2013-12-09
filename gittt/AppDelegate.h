e915c8bbfd993641c5393efaaa8abf54484bc1ff
wrs <252797991@qq.com>
2013-12-09 13:54:49 +0800

    Initial Commit

---
+++ gittt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gittt
+//
+//  Created by athudong on 13-12-9.
+//  Copyright (c) 2013年 athudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
